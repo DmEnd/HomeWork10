548c854bd6250d86191233c95990894925325429
DmEnd <Dima_polit-sov@mail.ru>
2022-11-06 21:06:54 +0300

    Добавьте файлы проекта.

---
+++ HomeWork10/Angle.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork10
+{
+    internal class Angle
+    {
+        //Поля
+        int gradus;
+        int min;
+        int sec;
+
+        //Cвойства
+        public int Gradus
+        {
+            set
+            {
+
+                if( 360< value )
+                {
+                    gradus = value % 360;
+                }
+                else if(value < -360)
+                {
+                    value = value % 360;
+                    gradus = value + 360;
+                }
+                else if(value < 0)
+                {
+                    gradus = value + 360;
+                }
+                else
+                {
+                    gradus = value;
+                }
+            }
+            get
+            {
+                return gradus;
+            }
+        }
+
+        public int Min
+        {
+            set
+            {
+                if (60 < value)
+                {
+                    Gradus+= value / 60;
+                    min = value % 60;
+                }
+                else if (value < -60)
+                {
+                    Gradus += value / 60-1;
+                    min = 60+value % 60;  
+                }
+                else if (value < 0)
+                {
+                    Gradus--;
+                    min = 60 + value % 60;
+                }
+                else
+                {
+                    min = value;
+                }
+            }
+            get
+            {
+                return min;
+            }
+        }
+
+        public int Sec
+        {
+            set
+            {
+                if (60 < value)
+                {
+                    Min += value / 60;
+                    sec = value % 60;
+                }
+                else if (value < -60)
+                {
+                    Min += value / 60 - 1;
+                    sec = 60 + value % 60;
+                }
+                else if (value < 0)
+                {
+                    Min--;
+                    min = 60 + value % 60;
+                }
+                else
+                {
+                    sec = value;
+                }
+            }
+            get
+            {
+                return sec;
+            }
+        }
+        //Конструктор
+        public Angle(int gradus, int min, int sec)
+        {
+            Gradus = gradus;
+            Min = min;
+            Sec = sec;
+        }
+        //Метод
+        public double ToRadians()
+        {
+            return ((double)gradus + (double)min / 60 + (double)sec / 60 / 60)/360*Math.PI;
+        }
+
+    }
+}
---
+++ HomeWork10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.Угол задан с помощью целочисленных значений gradus -градусов, min - угловых минут, sec - угловых секунд.Реализовать 
+            * класс, в котором указанные значения представлены в виде свойств.Для свойств предусмотреть проверку корректности данных. 
+            *Класс должен содержать конструктор для установки начальных значений, а также метод ToRadians для перевода угла в радианы. 
+            *Создать объект на основе разработанного класса. Осуществить использование объекта в программе.
+            */
+            Console.Write("Введите градусы угла = ");
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите минуты угла = ");
+            int min = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите секунды угла = ");
+            int sec = Convert.ToInt32(Console.ReadLine());
+
+            //int gradus = 360;
+            //int min = -70;
+            //int sec = -70;
+
+            Angle angle = new Angle(gradus, min, sec);
+
+            Console.WriteLine("Угол {0}° {1}\" {2}\' в радианах равен {3}", angle.Gradus, angle.Min, angle.Sec, angle.ToRadians());
+            Console.ReadKey();
+        }
+    }
+}
+    
